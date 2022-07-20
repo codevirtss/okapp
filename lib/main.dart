9311932a44ea67b0fdbf5a4dd686b17f7d42b8ca
Raza Khan <thisisrazakhan8@gmail.com>
2022-07-21 03:04:33 +0530

    working on splash

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:okapp/Screens/MainPage.dart';
+import 'package:okapp/Screens/Spalsh1.dart';
 import 'package:okapp/Screens/reportEvent.dart';
 import 'package:okapp/Screens/shareTheApp.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
